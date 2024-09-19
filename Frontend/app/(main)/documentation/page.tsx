ed7609bd2384da3c9bc1b465aa66c8b7985e4a52
Cuddles47 <binbill472003@gmail.com>
2024-09-19 13:37:43 +0700

    upload code

---
+++ Frontend/app/(main)/documentation/page.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable @next/next/no-sync-scripts */
+import React from 'react';
+
+const Setting = () => {
+    return (
+        <>
+            
+        </>
+    );
+};
+
+export default Setting;
---
+++ Frontend/layout/AppMenu.tsx
@@ -0,0 +1,62 @@
+/* eslint-disable @next/next/no-img-element */
+
+import React, { useContext } from 'react';
+import AppMenuitem from './AppMenuitem';
+import { LayoutContext } from './context/layoutcontext';
+import { MenuProvider } from './context/menucontext';
+import Link from 'next/link';
+import { AppMenuItem } from '@/types';
+
+const AppMenu = () => {
+    const { layoutConfig } = useContext(LayoutContext);
+
+    const model: AppMenuItem[] = [
+        {
+            label: 'Home',
+            items: [{ label: 'Dashboard', icon: 'pi pi-fw pi-home', to: '/' }]
+        },
+        
+        {
+            label: 'Pages',
+            icon: 'pi pi-fw pi-briefcase',
+            to: '/pages',
+            items: [
+                
+                {
+                    label: 'Auth',
+                    icon: 'pi pi-fw pi-user',
+                    items: [
+                        {
+                            label: 'Login',
+                            icon: 'pi pi-fw pi-sign-in',
+                            to: '/auth/login'
+                        },
+                    ]
+                },
+                {
+                    label: 'Crud',
+                    icon: 'pi pi-fw pi-pencil',
+                    to: '/pages/crud'
+                },
+                {
+                    label: 'My Account',
+                    icon: 'pi pi-fw pi-circle-off',
+                    to: '/pages/user-account'
+                }
+            ]
+        },
+        
+    ];
+
+    return (
+        <MenuProvider>
+            <ul className="layout-menu">
+                {model.map((item, i) => {
+                    return !item?.seperator ? <AppMenuitem item={item} root={true} index={i} key={item.label} /> : <li className="menu-separator"></li>;
+                })}
+            </ul>
+        </MenuProvider>
+    );
+};
+
+export default AppMenu;
