ceef07c03e01116be8eb29ed6a4a22c95301672b
_kewwi ng_ <102494428+cuddles47@users.noreply.github.com>
2024-09-23 02:55:39 +0700

    Delete Frontend directory

--- Frontend/src/components/Crud.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Crud = () => <h1>Admin Crud Page</h1>;
-
-export default Crud;
--- Frontend/src/components/UserAccount.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const  UserAccount = () => <h1>User Account</h1>;
-
-export default UserAccount;
